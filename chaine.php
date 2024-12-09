4dcd37c81d77b73a71f76623b84587cb41c0b75f
al3x6 <alexisar2002@gmail.com>
2024-12-09 23:08:24 +0100

    page importance de bien entretenir sa moto

--- chaine.php
+++ chaine.php
@@ -10,13 +10,15 @@
 <?php include("import/navigation.php"); ?>
 
 <main>
-    <h1>Entretenir une chaîne</h1>
-    <div class="content">
-        <video src="videos/chaine.mp4" controls></video>
-        <div class="text">
-            <p>Une chaîne mal entretenue peut causer des accidents. Nettoyez-la régulièrement avec un dégraissant et appliquez un lubrifiant adapté pour éviter l'usure prématurée.</p>
+    <section class="chaine">
+        <h1>Entretenir une chaîne</h1>
+        <div>
+            <video src="videos/chaine.mp4" controls></video>
+            <div class="text">
+                <p>Une chaîne mal entretenue peut causer des accidents. Nettoyez-la régulièrement avec un dégraissant et appliquez un lubrifiant adapté pour éviter l'usure prématurée.</p>
+            </div>
         </div>
-    </div>
+    </section>
 </main>
 
 <?php include("import/footer.php"); ?>
--- importance.php
+++ importance.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L'importance de bien entretenir sa moto</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styless.css">
 </head>
 <body>
 <?php include("import/navigation.php"); ?>
 
-<main>
-    <h1>L'importance de bien entretenir sa moto</h1>
-    <div class="content">
-        <video src="videos/importance.mp4" controls></video>
-        <div class="text">
-            <p>Entretenir sa moto régulièrement prolonge sa durée de vie et assure votre sécurité. Que ce soit vérifier la pression des pneus ou changer l'huile, chaque détail compte. Découvrez ici pourquoi l'entretien est crucial pour chaque motard.</p>
+<main class="main_importance">
+    <section class="importance">
+        <div class="overlay">
+            <h1>L'importance de bien entretenir sa moto</h1>
+            <p>
+                Un bon entretien garantit que tous les systèmes de la moto fonctionnent correctement,
+                ce qui réduit le risque d'accidents dus à des défaillances mécaniques.
+                Une moto bien entretenue offre de meilleures performances. Cela inclut
+                une meilleure accélération, une meilleure maniabilité et une consommation
+                de carburant optimisée. Un entretien régulier prolonge la durée de vie de
+                la moto...
+            </p>
         </div>
-    </div>
 </main>
 
 <?php include("import/footer.php"); ?>
