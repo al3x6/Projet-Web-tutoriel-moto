ad37cf20ba4fcd42a4bc141922aab0f7841289fe
al3x6 <alexisar2002@gmail.com>
2024-12-12 17:33:20 +0100

    changement d'extension fichier vers html

--- import/navigation.php
+++ import/navigation.php
@@ -1,12 +1,12 @@
 <header>
 <nav>
     <ul>
-        <li><a href="../index.php">Accueil</a></li>
-        <li><a href="../importance.php">L'importance de bien entretenir sa moto</a></li>
-        <li><a href="../chaine.php">Entretenir une chaîne</a></li>
-        <li><a href="../vidange.php">Faire une vidange</a></li>
-        <li><a href="../ampoule.php">Changer une ampoule</a></li>
-        <li><a href="../freins.php">Changer les plaquettes de frein</a></li>
+        <li><a href="../index.html">Accueil</a></li>
+        <li><a href="../importance.html">L'importance de bien entretenir sa moto</a></li>
+        <li><a href="../chaine.html">Entretenir une chaîne</a></li>
+        <li><a href="../vidange.html">Faire une vidange</a></li>
+        <li><a href="../ampoule.html">Changer une ampoule</a></li>
+        <li><a href="../freins.html">Changer les plaquettes de frein</a></li>
     </ul>
 </nav>
 </header>
